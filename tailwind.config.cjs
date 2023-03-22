31b11f023d3566f587a90b4b7802aee41ff280b0
Esteban Portillo <esteban.07.elias@gmail.com>
2023-03-22 16:09:20 -0600

    Create new media query for tablet screens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        tablet: { max: '800px' },
+      },
       gridTemplateColumns: {
         'fit-60': 'repeat(auto-fit, 15rem)',
       },

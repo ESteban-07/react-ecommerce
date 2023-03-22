8112945b9023e880626e4a1dad7be896b43c927d
Esteban Portillo <esteban.07.elias@gmail.com>
2023-03-22 15:49:56 -0600

    Add margin top and bottom to MainGrid Component

--- src/components/MainGrid.jsx
+++ src/components/MainGrid.jsx
@@ -5,7 +5,7 @@ function MainGrid() {
   const { products } = useProductsContext();
 
   return (
-    <div className="grid grid-cols-fit-60 place-content-center gap-7">
+    <div className="grid grid-cols-fit-60 place-content-center gap-7 my-7">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}

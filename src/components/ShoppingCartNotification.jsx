2034e0db84adc687b773603ebb64da0af4f285f1
Esteban Portillo <esteban.07.elias@gmail.com>
2023-03-22 16:12:19 -0600

    Start working on shopping cart

--- src/components/ShoppingCartNotification.jsx
+++ src/components/ShoppingCartNotification.jsx
@@ -1,20 +1,29 @@
-import ShoppingCartIcon from '../assets/icons/icon_shopping_cart_notification.svg';
+import { useState } from 'react';
 import { useProductsContext } from '../hooks/useProductsContext';
+import ScreenShoppingCart from '../screens/ScreenShoppingCart';
+import ShoppingCartIcon from '../assets/icons/icon_shopping_cart_notification.svg';
 
 function ShoppingCartNotification() {
-  const { cartCounterValue } = useProductsContext();
+  const { cartCounterValue, cartIconRef } = useProductsContext();
+  const [display, setDisplay] = useState(false);
 
   return (
-    <div className="relative w-10">
-      <span className="absolute top-[5.5px] right-[5px] text-[12px] leading-[0] font-bold">
-        {cartCounterValue}
-      </span>
-      <img
-        className="w-full h-full object-cover"
-        src={ShoppingCartIcon}
-        alt="Shopping cart notifications"
-      />
-    </div>
+    <>
+      <div
+        ref={cartIconRef}
+        className="relative w-10 cursor-pointer"
+        onClick={() => setDisplay((current) => !current)}>
+        <span className="absolute top-[5.5px] right-[5px] text-[12px] leading-[0] font-bold">
+          {cartCounterValue}
+        </span>
+        <img
+          className="w-full h-full object-cover"
+          src={ShoppingCartIcon}
+          alt="Shopping cart notifications"
+        />
+      </div>
+      {display && <ScreenShoppingCart />}
+    </>
   );
 }
 
--- src/screens/ScreenShoppingCart.jsx
+++ src/screens/ScreenShoppingCart.jsx
@@ -1,35 +1,37 @@
 import { useProductsContext } from '../hooks/useProductsContext';
 import RemoveItemFromCartIcon from '../assets/icons/icon_close.png';
 
-function ShoppingCart() {
+function ScreenShoppingCart() {
   const { currency, cartProducts, totalPayment, toggleItemFromCart } =
     useProductsContext();
 
   return (
-    <div>
-      <h1>Shopping Cart Products ({cartProducts.length}) 🛒</h1>
-      <ul>
-        {cartProducts.map((item, idx) => {
-          return (
-            <li key={idx}>
-              <span>
-                {item.title} - {currency.format(item.price)}
-              </span>
-              <button
-                className="border border-solid border-slate-600 px-2 py-2 mx-2 bg-transparent rounded"
-                onClick={() => toggleItemFromCart(item)}>
-                <img
-                  src={RemoveItemFromCartIcon}
-                  alt={`Remove ${item.title} from cart`}
-                />
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-      <p>Total: {currency.format(totalPayment)}</p>
-    </div>
+    <aside className="absolute top-[70px] right-0 bg-white shadow-md w-[360px] h-[550px]">
+      <div className="">
+        <h1>Shopping Cart Products ({cartProducts.length}) 🛒</h1>
+        <ul>
+          {cartProducts.map((item, idx) => {
+            return (
+              <li key={idx}>
+                <span>
+                  {item.title} - {currency.format(item.price)}
+                </span>
+                <button
+                  className="border border-solid border-slate-600 px-2 py-2 mx-2 bg-transparent rounded"
+                  onClick={() => toggleItemFromCart(item)}>
+                  <img
+                    src={RemoveItemFromCartIcon}
+                    alt={`Remove ${item.title} from cart`}
+                  />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <p>Total: {currency.format(totalPayment)}</p>
+      </div>
+    </aside>
   );
 }
 
-export default ShoppingCart;
+export default ScreenShoppingCart;

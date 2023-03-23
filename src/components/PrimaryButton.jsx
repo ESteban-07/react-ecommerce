6b404dab0e0522b9d1999885d614a6a8db7180f3
Esteban Portillo <esteban.07.elias@gmail.com>
2023-03-23 11:45:45 -0600

    Make ScreenShoppingCart responsive and create new component PrimaryButton

---
+++ src/components/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+function PrimaryButton({ title }) {
+  return (
+    <button className="h-[50px] bg-[#acd9b2] text-white text-center capitalize font-bold border-none rounded-[10px]">
+      {title}
+    </button>
+  );
+}
+
+export default PrimaryButton;
--- src/screens/ScreenShoppingCart.jsx
+++ src/screens/ScreenShoppingCart.jsx
@@ -1,54 +1,66 @@
 import { useProductsContext } from '../hooks/useProductsContext';
 import RemoveItemFromCartIcon from '../assets/icons/icon_close.png';
+import EmptyCartIcon from '../assets/icons/icon_shopping_cart.svg';
+import PrimaryButton from '../components/PrimaryButton';
 
 function ScreenShoppingCart() {
   const { currency, cartProducts, totalPayment, toggleItemFromCart } =
     useProductsContext();
 
   return (
-    <aside className="absolute top-[70px] right-0 bg-white shadow-md w-[360px] h-[550px] mobile:w-screen">
-      <div className="w-full h-[inherit] flex flex-col justify-between ">
-        <h1>Shopping Cart Products ({cartProducts.length}) 🛒</h1>
+    <aside className="grid mobile:justify-center absolute top-[70px] right-0 bg-white shadow-md w-[360px] min-h-[550px] max-h-[calc(100vh-68px)] mobile:w-screen ">
+      <div className="w-full mobile:w-[360px] h-[inherit] flex flex-col justify-between ">
+        <div className="p-4">
+          <h1 className="font-bold text-[18px]">Shopping Cart</h1>
+        </div>
 
         <div
           id="cart-items"
-          className="flex flex-col min-h-[335px] max-h-[500px] overflow-y-scroll scrollbar-hide">
-          {cartProducts.map((item, idx) => {
-            return (
-              <div
-                id="cart-item"
-                className="flex justify-between items-center p-4"
-                key={idx}>
-                <figure className="flex items-center gap-4">
-                  <img
-                    className="w-[70px] h-[70px] rounded-[10px] object-cover"
-                    src={item.images[0]}
-                    alt={`Image of ${item.title}`}
-                  />
-                  <figcaption>{item.title}</figcaption>
-                </figure>
-                <div className="flex gap-2">
-                  <p>{currency.format(item.price)}</p>
-                  <button
-                    className="border border-solid border-slate-600 px-2 py-2 bg-transparent rounded"
-                    onClick={() => toggleItemFromCart(item)}>
+          className="flex flex-col h-[360px] max-h-[500px] overflow-y-scroll scrollbar-hide">
+          {cartProducts.length === 0 ? (
+            <img
+              className="w-[80%] aspect-[1] self-center color-hospital-green"
+              src={EmptyCartIcon}
+              alt="Cart is empty"
+            />
+          ) : (
+            cartProducts.map((item, idx) => {
+              return (
+                <div
+                  id="cart-item"
+                  className="flex justify-between items-center p-4"
+                  key={idx}>
+                  <figure className="flex items-center gap-4">
                     <img
-                      src={RemoveItemFromCartIcon}
-                      alt={`Remove ${item.title} from cart`}
+                      className="w-[70px] h-[70px] rounded-[10px] object-cover"
+                      src={item.images[0]}
+                      alt={`Image of ${item.title}`}
                     />
-                  </button>
+                    <figcaption>{item.title}</figcaption>
+                  </figure>
+                  <div className="flex gap-2">
+                    <p>{currency.format(item.price)}</p>
+                    <button
+                      className="border border-solid border-slate-600 px-2 py-2 bg-transparent rounded"
+                      onClick={() => toggleItemFromCart(item)}>
+                      <img
+                        src={RemoveItemFromCartIcon}
+                        alt={`Remove ${item.title} from cart`}
+                      />
+                    </button>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          )}
         </div>
 
-        <div id="total-payment" className="flex flex-col">
-          <div className="flex justify-between">
+        <div id="total-payment" className="flex flex-col p-4 gap-4">
+          <div id="total" className="flex justify-between">
             <p>Total</p>
             <p className="font-bold">{currency.format(totalPayment)}</p>
           </div>
-          {/* <button>Checkout</button> */}
+          <PrimaryButton title="checkout" />
         </div>
       </div>
     </aside>

5f70f5de8b67a4bb5b9cb2084f3415a459ffaa80
Esteban Portillo <esteban.07.elias@gmail.com>
2023-03-23 14:01:13 -0600

    ScreenShoppingCart fixed to mobile viewport

--- src/components/ShoppingCartNotification.jsx
+++ src/components/ShoppingCartNotification.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useProductsContext } from '../hooks/useProductsContext';
 import ScreenShoppingCart from '../screens/ScreenShoppingCart';
 import ShoppingCartIcon from '../assets/icons/icon_shopping_cart_notification.svg';
@@ -7,12 +7,21 @@ function ShoppingCartNotification() {
   const { cartCounterValue, cartIconRef } = useProductsContext();
   const [display, setDisplay] = useState(false);
 
+  // ScreenShoppingCart fixed to viewport
+  useEffect(() => {
+    display
+      ? document.body.classList.add('mobile-l:overflow-hidden')
+      : document.body.classList.remove('mobile-l:overflow-hidden');
+  }, [display]);
+
   return (
     <>
       <div
         ref={cartIconRef}
         className="relative w-10 cursor-pointer"
-        onClick={() => setDisplay((current) => !current)}>
+        onClick={() => {
+          setDisplay((current) => !current);
+        }}>
         <span className="absolute top-[5.5px] right-[5px] text-[12px] leading-[0] font-bold">
           {cartCounterValue}
         </span>

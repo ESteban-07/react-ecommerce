d259568422d048355e2c09a8a3fc5f7fb3bc0230
Esteban Portillo <esteban.07.elias@gmail.com>
2023-03-23 18:11:18 -0600

    Implement absolute import path statements and save as a dependency

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig({
   base: '/react-ecommerce/',
-  plugins: [react()],
+  plugins: [react(), jsconfigPaths()],
 });

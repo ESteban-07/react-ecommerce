91e48f4b3a6d90f72659a56f5988abf42311c1b6
Esteban Portillo <esteban.07.elias@gmail.com>
2023-03-23 18:10:29 -0600

    Relocate components to respective directory

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import ShoppingCartNotification from './ShoppingCart/ShoppingCartNotification';
+import YardSaleLogo from '../assets/logos/logo_yard_sale.svg';
+import BurguerMenuIcon from '../assets/icons/icon_menu.svg';
+
+function Header() {
+  return (
+    <header className="relative flex justify-between items-center w-full p-4 shadow-sm">
+      <div className="hidden tablet:block">
+        <img src={BurguerMenuIcon} alt="Burguer Menu Icon" />
+      </div>
+      <div id="logo-links" className="flex justify-center items-center gap-4">
+        <div>
+          <img src={YardSaleLogo} alt="Yard sale logo" />
+        </div>
+        <nav className="tablet:hidden">
+          <ul className="flex gap-4">
+            <li>Clothes</li>
+            <li>Electronics</li>
+            <li>Furniture</li>
+            <li>Toys</li>
+            <li>Others</li>
+          </ul>
+        </nav>
+      </div>
+      <div id="views" className="flex justify-center items-center gap-4">
+        <p className="tablet:hidden">platzi@example.com</p>
+        <ShoppingCartNotification />
+      </div>
+    </header>
+  );
+}
+
+export default Header;

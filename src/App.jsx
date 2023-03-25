e201333382ad28d8b257b1cc89bd71336875d326
Esteban Portillo <esteban.07.elias@gmail.com>
2023-03-25 13:52:55 -0600

    Modify App to include ScreenProductDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import MainGrid from 'components/ProductsGrid/MainGrid';
 import Header from 'components/Header/Header';
+import ScreenProductDetails from 'screens/ScreenProductDetails';
+import { useProductsContext } from 'hooks/useProductsContext';
 
 function App() {
+  const { isOpened } = useProductsContext();
+
   return (
     <>
       <Header />
       <MainGrid />
+      {isOpened && <ScreenProductDetails />}
     </>
   );
 }
